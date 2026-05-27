94d30ee7bf8e013476453a1cdfce751d239f4a25
asmitbiswas <asmitbiswas1308@gmail.com>
2026-05-27 12:34:38 +0530

    Dynamic routing

--- Dynamic_Routing/app/app/page.js
+++ Dynamic_Routing/app/app/page.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 export default async function Home(searchParams, params) {
-  console.log(searchParams)
-  console.log(params)
+  console.log(await searchParams)
+  console.log(await params)
   return(
     <div>
     <h1>Technical Agency</h1>
