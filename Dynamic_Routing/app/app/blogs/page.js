2bd4ec47ba59ba445600ea2da9d0cf04fe07b32e
asmitbiswas <asmitbiswas1308@gmail.com>
2026-05-27 11:51:13 +0530

    Dynamic routing

---
+++ Dynamic_Routing/app/app/blogs/page.js
@@ -0,0 +1,18 @@
+const { default: Link } = require("next/link");
+
+export default function servicess(){
+    return(
+        <div>
+            <h1>
+                <p>
+                    <Link href = "/blogs/web-dev">Web development</Link>
+                </p>
+                <p>
+                    <Link href="/blogs/seo">SEO</Link>
+                </p>
+
+                <Link href="/blogs/about">about</Link>
+            </h1>
+        </div>
+    )
+}
---
+++ Dynamic_Routing/app/app/blogs/seo/page.js
@@ -0,0 +1,7 @@
+export default function servicess(){
+    return(
+        <div>
+            <h1>SEO services</h1>
+        </div>
+    )
+}
