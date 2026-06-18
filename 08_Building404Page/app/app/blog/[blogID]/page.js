5eb197aba7a612db1a2c5d3442aad26ad6e70370
asmitbiswas <asmitbiswas1308@gmail.com>
2026-06-18 12:29:57 +0530

    Feature: Customizable Error page

--- 08_Building404Page/app/app/blog/[blogID]/page.js
+++ 08_Building404Page/app/app/blog/[blogID]/page.js
@@ -1,11 +1,16 @@
-export async function GenerateMetaData(params) {
+import { notFound } from "next/navigation";
+
+export async function GenerateMetaData({params}) {
     const {blogID} = await params;
     return{
         title: `Blog ${blogID}`,
     }
 }
 export default async function Blog({params}){
-    const {blogID} = await params
+    const {blogID} = await params;
+       if (blogID === 'test') {
+        notFound();
+    }
     return(
         <div>Blog {blogID}</div>
     )
