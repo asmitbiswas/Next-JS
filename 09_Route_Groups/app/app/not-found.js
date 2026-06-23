62cdefe1d608e41b085e7e2d47077f3ed839e7d2
asmitbiswas <asmitbiswas1308@gmail.com>
2026-06-23 13:29:54 +0530

    Updated not-found code

--- 09_Route_Groups/app/app/not-found.js
+++ 09_Route_Groups/app/app/not-found.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function () {
   return (
     <>
       <h1>Page not found!</h1>
       <p>Could not found the page you are looking for.</p>
+      <p>
+        <Link href ="/home">Go back to home</Link>
+      </p>
     </>
   );
 }
